41c59e904d0a5067182b2712e85a15e16fa5975e
coolistkovbel <coolistkovbel@gmail.com>
2022-06-15 22:33:51 -0400

    finished going to try to do the stretch

--- index.js
+++ index.js
@@ -56,8 +56,19 @@ Person.prototype.toString = function(){
 */
 
 function Car(model, milesPerGallon) {
-
+  this.model = model;
+  this.milesPerGallon = milesPerGallon;
+  this.tank = 0
+  this.odometer = 0
 }
+
+Car.prototype.fill = function(gallons){
+  return this.tank += gallons;
+}
+
+// Car.prototype.drive = function(distance) {
+//   this.milesPerGallon = this.tank
+// }
 
 
 /*
@@ -68,10 +79,16 @@ function Car(model, milesPerGallon) {
         + Should return a string "Playing with x", x being the favorite toy.
 */
 
-function Baby() {
-
+function Baby(name, age,favoriteToy) {
+  Person.call(this, name, age);
+  this.favoriteToy = favoriteToy
 }
 
+Baby.prototype = Object.create(Person.prototype)
+
+Baby.prototype.play = function(){
+  return `Playing with ${this.favoriteToy}`
+}
 
 /* 
   TASK 4
